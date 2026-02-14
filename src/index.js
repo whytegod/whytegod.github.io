d1b2afc0e356f1801e5fbce812938776424c37cd
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-15 06:26:56 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-g
+/*
+ * Copyright © 2026 WGD Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * WGD™ is a trademark of WGD Foundation.
+ */
+
+function wgd() {
+  return "WGD Core is active.";
+}
+
+module.exports = {
+  wgd
+};
